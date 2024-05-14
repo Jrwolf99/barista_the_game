b1992339a67653a6e61eeb9472f4aa9866a5fb09
Jonathan Wolf <jonathan.wolf@showplacehq.com>
2024-05-13 23:34:17 -0500

    clean up

--- app/game/GameBoard.jsx
+++ app/game/GameBoard.jsx
@@ -1,33 +1,27 @@
 'use client';
 import React from 'react';
-
-import { useOrderGenerator } from '@/app/game/useOrderGenerator';
 import Station from '@/app/game/Station';
-import { useControlGame } from '@/app/game/useControlGame';
-import Image from 'next/image';
 
-export default function GameBoard({ setScoreMoney }) {
-  const { currentOrder } = useControlGame(setScoreMoney);
-
+export default function GameBoard({ currentOrder }) {
   return (
     <div className=" flex flex-row justify-center items-start gap-5 my-5">
-      <div>
-        <p className="text-2xl text-white text-center">
-          {' '}
-          A {currentOrder?.hot_or_iced} {currentOrder?.size}
-          <br /> drink for {currentOrder?.customer_name}
+      <div className="w-[200px]">
+        <p className="text-xl text-white text-center max-w-[200px] h-[75px] mb-5">
+          {currentOrder?.size} {currentOrder?.drink_name}
+          <br />
+          for {currentOrder?.customer_name}
         </p>
-        <div className="relative bg-[#fafafa] border-2 border-white rounded-lg mx-auto w-[150px] h-[600px]">
+        <div className="relative bg-[#fafafa] border-2 border-white rounded-lg mx-auto w-[100px] h-[400px]">
           <img
             src="/cup_order.png"
             alt="coffee cup"
-            className="object-cover h-[600px] opacity-10"
+            className="object-cover h-[100%] opacity-20"
           />
           <div className="text-black font-bold">
-            <p className="absolute top-[235px] left-[55px] text-2xl">
+            <p className="absolute top-[150px] left-[40%] text-xl">
               {currentOrder?.syrup_symbol}
             </p>
-            <p className="absolute top-[530px] left-[50px] text-2xl">
+            <p className="absolute top-[345px] left-[35%] text-xl">
               {currentOrder?.symbol}
             </p>
           </div>
